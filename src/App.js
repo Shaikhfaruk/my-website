d1458e5b81a1372e3af8af3b7b1a68238533a151
Shaikhfaruk <sfaruk1137b@gmail.com>
2021-03-14 22:05:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <footer id="footer">
+        <div class="main">
+          <div class="icon">
+            <svg height="80" width="80">
+              <circle
+                cx="20"
+                cy="20"
+                r="15"
+                stroke="white"
+                stroke-width="2"
+                fill="none"
+              />
+            </svg>
+            <i class="fab fa-instagram fa-1x" />
+          </div>
+
+          <div class="icon">
+            <svg height="80" width="80">
+              <circle
+                cx="20"
+                cy="20"
+                r="15"
+                stroke="white"
+                stroke-width="2"
+                fill="none"
+              />
+            </svg>
+            <i class="fab fa-facebook fa-1x" />
+          </div>
+
+          <div class="icon">
+            <svg height="80" width="80">
+              <circle
+                cx="20"
+                cy="20"
+                r="15"
+                stroke="white"
+                stroke-width="2"
+                fill="none"
+              />
+            </svg>
+            <i class="fab fa-whatsapp fa-1x" />
+          </div>
+
+          <div class="icon">
+            <svg height="80" width="80">
+              <circle
+                cx="20"
+                cy="20"
+                r="15"
+                stroke="white"
+                stroke-width="2"
+                fill="none"
+              />
+            </svg>
+            <i class="fab fa-telegram fa-1x" />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
